c634ace1d078a9a3986d7c2afa7f21b40948e6b0
fanjing <fanjing_23143018@163.com>
2017-06-30 11:19:54 +0800

    create project

---
+++ FJFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FJFramework
+//
+//  Created by 樊静 on 2017/6/30.
+//  Copyright © 2017年 樊静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
